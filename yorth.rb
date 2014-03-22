3dd07c8fce4b7dd9f085dd201492ca98c16a28c8
Andrew <quadcorder@gmail.com>
2014-03-22 10:59:34 -0400

    Updated to latest dropbox version

--- yorth.rb
+++ yorth.rb
@@ -1,24 +1,23 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 require 'readline'
 class YorthError < Exception;			end
 class YorthTypeError < YorthError;		end
 class YorthArgumentError < YorthError;	end
 class YorthNameError < YorthError;		end
+class YorthSyntaxError < YorthError;	end
 class YorthData
 	attr_reader	:value
 	def initialize kind = nil, value = nil
 		@kind = kind
 		@value = value
 	end
-	def dup;	Marshal.load(Marshal.dump(self))	end
-	def to_s;		@value.to_s;					end
+	def dup;	Marshal.load(Marshal.dump self)	end
+	def to_s;	@value.to_s;						end
 	def to_i
-		raise YorthTypeError.new("cannot convert #{value.class} #{@value} to an int") unless @value.to_i.to_s == @value.to_s
+		raise YorthTypeError.new("cannot convert #{value.class} #{@value} to an int") unless @value.respond_to? :to_i and @value.to_i.to_s == @value.to_s
 		@value.to_i
 	end
-	def inspect
-		[@kind, @value].inspect
-	end
+	def inspect;	[@kind, @value].inspect;		end
 	def + other
 		begin
 			@value + other.coerce(self)
@@ -36,26 +35,37 @@ class YorthData
 		end
 	end
 end
-class YorthArray < YorthData
-	def initialize strings = [], enclosure = Code.new
-		@value = []
+class YorthArray
+	def initialize values = []
+		@values = values
+	end
+# List of closures
+	def evaluate strings, enclosure = Closure.new
 		while strings.include?(',')
 			comma = strings.index(',')
 			strings.slice! comma
-			@value << Closure.new(strings.slice!(0,comma), enclosure).draw
+			item = Closure.new(strings.slice!(0,comma).reverse, enclosure)
+			item.parse!
+			@values << item.apply(Closure.new, :block)
 		end
-		@value << Closure.new(strings, enclosure).draw
+		item = Closure.new(strings.reverse, enclosure)
+		item.parse!
+		@values << item.apply(Closure.new, :block)
+	end
+	def to_s;		@values.to_s;		end
+	def to_a;		@values;			end
+	def inspect;	@values.inspect;	end
+	def + other
+		raise YorthTypeError.new("cannot add array #{@values.inspect} and #{other.class} #{other}") unless other.is_a? YorthArray
+		YorthArray.new(@values + other.to_a)
 	end
 end
 class YorthString
-	def initialize value = "";	@value = value.to_s		end
-	def dup;					YorthString.new @value	end
-	def inspect;				@value.inspect			end
-	def to_s;					@value					end
-	def to_i
-		nil unless @value.to_i.to_s == @value.to_s
-		@value.to_i
-	end
+	def initialize value;	@value = value.to_s				end
+	def dup;				YorthString.new @value			end
+	def inspect;			@value.inspect					end
+	def to_s;				@value							end
+	def to_i;	@value.to_i if @value.to_i.to_s == @value	end
 	def + other
 		raise YorthTypeError.new("cannot concatenate YorthString #{@value} and #{other.class} #{other}") unless other.is_a? YorthString
 		YorthString.new @value + other.to_s
@@ -68,11 +78,13 @@ class Closure
 		@scope = Hash[]
 		@caller = caller
 	end
-	def to_s;		@code.to_s;						end
-	def to_i;		nil;							end
-	def inspect;	[@code, @scope].inspect;		end
-	def terminates;	@code == [];					end
-	def dup caller = Closure.new([], nil); Closure.new(Marshal.load(Marshal.dump(@code)), @enclosure, caller)	end
+	def to_s;		@code.to_s;							end
+	def inspect;	[@code.reverse, @scope].inspect;	end
+	def terminates;	@code == [];						end
+	def dup caller = Closure.new([], nil)
+		Closure.new(Marshal.load(Marshal.dump(@code)), @enclosure, caller)
+	end
+# Fix duplication to ensure proper superscope, fix variable location, resolution
 	def has? word
 		return true unless @scope[word].nil?
 		return false if @enclosure.nil?
@@ -88,14 +100,8 @@ class Closure
 		nil
 	end
 	def unassign word
-		if not @scope[word].nil?
-			@scope[word] = nil
-			nil
-		elsif @enclosure.nil?
-			nil
-		else
-			@enclosure.unassign word
-		end
+		@scope[word] = nil			unless @scope[word].nil?
+		@enclosure.unassign word	unless @enclosure.nil?
 	end
 	def assign (word, value)
 		if @scope[word] != nil
@@ -119,17 +125,49 @@ class Closure
 				rescue Errno::ENOENT
 				end
 			end
-			begin
-				File.open(library).each do |line|
-					begin
-						evaluate line.split
-					rescue YorthError => error
-						puts "#{error.class}: #{error}"
+			libtype = library.split('.')[-1]
+			if libtype == "wye"
+				file = []
+				begin
+					File.open(library).each do |line|
+						code  = line.split
+						file += code unless code[0] == "//"
 					end
+					puts "'#{library}' loaded"
+				rescue Errno::ENOENT
+					puts "'#{arg}' does not exist"
 				end
-				puts "#{arg} loaded"
-			rescue Errno::ENOENT
-				puts "#{arg} does not exist"
+				begin
+					evaluate file
+				rescue YorthError => error
+					puts "#{error.class}: #{error}"
+				end
+			elsif libtype == "wyl"
+				file  = []
+				pairs = Hash[]
+				input = ""
+				begin
+					File.open(library).each do |line|
+						code = line.split
+						if code[0] == "<="
+							file.push(code[1..-1])
+							input   = code[1..-1]
+						elsif code[0] == "=>"
+							pairs[input.to_s] = line[3..-1]
+							input        = nil
+						end
+					end
+					puts "'#{library}' loaded"
+				rescue Errno::ENOENT
+					puts "'#{arg}' does not exist"
+				end
+				begin
+					verify(file, pairs)
+				rescue YorthError => error
+					puts "#{error.class}: #{error}"
+				end
+			else
+				puts "'#{library}' is a .#{libtype} file, not a .wye or .wyl file."
 			end
 		end
 		nil
@@ -137,8 +175,7 @@ class Closure
 	def interpret
 		puts "yorth interpreter initialized"
 		begin
-			print "<= "
-			evaluate Readline.readline("", true).chomp.split
+			evaluate Readline.readline("<= ", true).chomp.split
 		rescue YorthError => error
 			puts "#{error.class}: #{error}"
 		end while true
@@ -161,8 +198,7 @@ class Closure
 				stop_index = code[i..code.length].index("'")+i+2
 				raise YorthArgumentError.new("non-terminting string") if stop_index.nil?
 				return code.take(i) + [YorthString.new(code[i+1..stop_index-1].join(' '))] + code[stop_index+1..code.length]
-			when '{'
-				pos_f << i
+			when '{'	then pos_f << i
 			when '}'
 				raise YorthArgumentError.new("non-opened function") if pos_f.nil? or pos_f == []
 				beginpoint = pos_f.pop
@@ -174,22 +210,23 @@ class Closure
 					code.shift(1)
 					return beginning + [function] + code
 				end
-			when '['
-				pos_a << i
+			when '['	then pos_a << i
 			when ']'
 				raise YorthArgumentError.new("non-opened array") if pos_a.nil? or pos_a == []
 				beginpoint = pos_a.pop
 				if pos_f == []
 					beginning = code.shift(beginpoint)
 					code.shift(1)
-					array = YorthArray.new(code.shift(i-beginpoint-1).reverse, self)
+					array = YorthArray.new
+					array.evaluate(code.shift(i-beginpoint-1), self)
 					code.shift(1)
 					return beginning + [array] + code
 				end
-			end
+				end
 			i += 1
 		end
-		raise YorthArgumentError.new("non-terminating function") unless pos_f == [] or pos_f.nil?
+		raise YorthArgumentError.new("non-terminating function")	unless pos_f == []
+		raise YorthArgumentError.new("non-terminating array")		unless pos_a == []
 		code
 	end
 	def parse!
@@ -212,21 +249,65 @@ class Closure
 			end
 		end
 	end
+	def verify(file, pairs)
+		tested = 0
+		passed = 0
+		failed = 0
+		file.each do |code|
+			@code = code.reverse
+			parse!
+			until @code == []
+				begin
+					last = draw :nil
+					fileout = last.inspect unless last.nil?
+				rescue TypeError => error
+					raise YorthTypeError.new("#{error} in #{code}")
+				end
+			end
+			@code = pairs[code.to_s].split.reverse
+			parse!
+			until @code == []
+				begin
+					last = draw :nil
+					pairout = last.inspect unless last.nil?
+				rescue TypeError => error
+					raise YorthTypeError.new("#{error} in #{code}")
+				end
+			end
+			if fileout != pairout then
+				puts "Evaluated       #{code.join(' ')}"
+				puts "Got             #{fileout}"
+				puts "Should have got #{pairout}"
+				puts "TEST FAILED."
+				tested += 1
+				failed += 1
+			else
+				#puts "Evaluated       #{code.join(' ')}"
+				#puts "Got             #{fileout}"
+				#puts "Test passed."
+				tested += 1
+				passed += 1
+			end
+		end
+		puts "#{failed} failures in #{tested} tests."
+	end
+	def apply(caller = Closure.new, *args)
+		function = dup caller
+		result = function.draw args until function.terminates
+		result
+	end
+	def eval item, *args
+		args = args.flatten.uniq
+		return item if args.include? :block or not item.is_a? Closure
+		item.apply self
+	end
 	def draw *args
 		args = args.flatten.uniq
 		word = @code.pop
 		if word.respond_to? :to_i and word.to_i.to_s == word.to_s	then word.to_i
-		elsif word.is_a? YorthString	then word
-		elsif word.is_a? Closure
-			return word if args.include? :block
-			function = word.dup self
-			until function.terminates
-				result = function.draw
-			end
-			result
-		elsif has? word
-			@code << resolve(word)
-			draw args
+		elsif word.is_a? YorthString or word.is_a? YorthArray		then word
+		elsif word.is_a? Closure									then eval(word, args)
+		elsif has? word												then eval(resolve(word), args)
 		else			case word
 		when nil		then raise YorthArgumentError.new("ran out of values") unless args.include? :nil
 		when '='		then draw == draw
@@ -239,13 +320,15 @@ class Closure
 		when '.'		then puts draw
 		when '/'		then draw / draw
 		when 'bye'		then exit
-		when 'clear'	then @scope = Hash[]
+		when 'def'		then name = @code.pop; declare name; assign(name, @caller.draw(:block,args))
 		when 'del'		then unassign @code.pop
 		when 'false'	then false
 		when 'inspect'	then self
 		when 'let'		then declare @code.pop
 		when 'load'		then load @code.pop
-		when 'lsp'		then name = @code.pop; declare name; assign(name, @enclosure.draw)
+		when 'lst'		then name = @code.pop; declare name; assign(name, draw(:block))
+		when 'lsp'		then name = @code.pop; declare name; assign(name, @caller.draw(args))
+		when 'nil'		then nil
 		when 'not'		then not draw
 		when 'or'		then t1 = draw; t2 = draw; t1 or t2
 		when 'pop'		then @caller.draw args
@@ -256,36 +339,49 @@ class Closure
 			puts "#{item.class} #{item.inspect}"
 		when 'if'
 			condition = draw :block
-			whentrue = draw :block
+			whentrue  = draw :block
 			whenfalse = draw :block
-			if condition.is_a? Closure
-				@code << condition
-				condition = draw
-			end
-			if condition
-				if whentrue.is_a? Closure and not args.include? :block
-					@code << whentrue
-					draw args
-				else
-					whentrue
-				end
+			if eval(condition)
+				eval(whentrue, args)
 			else
-				if whenfalse.is_a? Closure and not args.include? :block
-					@code << whenfalse
-					draw args
-				else
-					whenfalse
-				end
+				eval(whenfalse, args)
 			end
+		when 'while'
+			condition  = draw :block
+			statements = draw :block
+			while eval condition
+				result = eval statements
+			end
+			result
+		when 'iter'
+			initial    = draw :block
+			condition  = draw :block
+			iterate    = draw :block
+			statements = draw :block
+			eval(initial)
+			while eval(condition)
+				result = eval(statements)
+				eval(iterate)
+			end
+			result
+		when 'enum'
+			statements = draw :block
+			list       = draw
+			result     = []
+			list.to_a.each do |item|
+				@code.unshift(item)
+				result << eval(statements)
+			end
+			result.compact
 		else raise YorthNameError.new("undefined word #{word.inspect}")
 		end
 		end
 	end
 end
-$libpath = if RUBY_PLATFORM.downcase.include? "linux"	then ["./lib",	"/usr/share/yorth/lib",	"/usr/lib/yorth/**",	"~/.yorth/lib/**"]
-		elsif RUBY_PLATFORM.downcase.include? "darwin"	then ["./lib",	"/usr/share/yorth/lib",	"/usr/lib/yorth/**",	"~/.yorth/lib/**"]
-		elsif RUBY_PLATFORM.downcase.include? "win"		then ["./lib",	"/Program Files/yorth/lib/**",					"~/.yorth/lib/**"]
-														else ["./lib",													"~/.yorth/lib/**"]
+$libpath = if RUBY_PLATFORM.downcase.include? "linux"	then [".",	"/usr/share/yorth/lib",	"/usr/lib/yorth/**",	"~/.local/share/yorth/lib"	"~/.yorth/lib/**"]
+		elsif RUBY_PLATFORM.downcase.include? "darwin"	then [".",	"/usr/share/yorth/lib",	"/usr/lib/yorth/**",	"~/Library/yorth"			"~/.yorth/lib/**"]
+		elsif RUBY_PLATFORM.downcase.include? "win"		then [".",	"/Program Files/yorth/lib/**", 					"%APPDATA%/yorth/**",		"~/.yorth/lib/**"]
+														else [".",																				"~/.yorth/lib/**"]
 end
 if inspect == "main"
 	main = Closure.new
